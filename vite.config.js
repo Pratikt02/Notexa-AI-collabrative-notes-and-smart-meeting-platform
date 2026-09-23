00d4de3f0a5fdbf32f5d5579ef7cc0c2960e7854
Pratikt02 <pratikbthange@gmail.com>
2026-09-23 11:45:11 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Notexa-AI-collabrative-notes-and-smart-meeting-platform/',
 })
